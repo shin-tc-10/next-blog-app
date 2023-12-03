9c0350b5bef0eccc568880a2e504aa63546308ca
shin-tc-10 <shin.nkt.09@gmail.com>
2023-12-03 16:14:21 +0900

    ヘッダー、フッターコンポーネント作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            ©︎2023 Next Blog App
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/app/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>記事を書く</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
