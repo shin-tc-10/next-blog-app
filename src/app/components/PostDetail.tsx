07b78204b9c6f8469e93c2b04e7e959f8faa73ff
shin-tc-10 <shin.nkt.09@gmail.com>
2024-02-14 22:33:56 +0900

    詳細記事への遷移, サムネイル画像表示

--- src/app/components/PostDetail.tsx
+++ src/app/components/PostDetail.tsx
@@ -1,16 +1,21 @@
 import { Post } from '@/types';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 type PostDetailProps = {
     post: Post;
 }
 
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const PostDetail = ({ post }: PostDetailProps) => {
     return (
         <div key={post.id}>
-            <p>{post.title}</p>
-            <p>{post.content}</p>
-            <p>{post.createdAt}</p>
+            <Link href={`${NEXT_PUBLIC_API_URL}/posts/${post.id}`}><Image src={`https://source.unsplash.com/collection/1346951/1000×300?sig=${post.id}`} alt="" width={1280} height={300} /></Link>
+            <Link href={`${NEXT_PUBLIC_API_URL}/posts/${post.id}`}><p>{post.title}</p></Link>
+            <Link href={`${NEXT_PUBLIC_API_URL}/posts/${post.id}`}><p>{post.content}</p></Link>
+            <Link href={`${NEXT_PUBLIC_API_URL}/posts/${post.id}`}><p>{post.createdAt}</p></Link>
         </div>
     )
 }
---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            aaaaa
+        </div>
+    )
+}
+
+export default page
